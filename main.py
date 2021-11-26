8d5d42a05a219ecc34c3200cb6e247d285f9ac86
zsamora <zsamora@ug.uchile.cl>
2021-11-25 21:34:04 -0300

    Versión prueba

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import sys
 import socket
-
 from tutorialHMI import *
-
 
 def send_command(text):
     try:
         s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-        s.connect(('98:d3:51:f5:c9:f4', 1))
+        s.connect(('98:d3:51:f5:c9:f4', 1)) # ID Bluetooth
         s.send(bytes(text, 'UTF-8'))
         s.close()
     except Exception as e:
@@ -20,7 +18,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         # Led and button
         self.led_state = False
+        self.rele_state = False
         self.pushButton.clicked.connect(self.change_led_state)
+        self.pushButton_2.clicked.connect(self.change_rele_state)
         # Dial and LCD
         self.dial.valueChanged.connect(self.slider_moved)
 
@@ -36,6 +36,19 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.label_2.setText("Secuencia LED: ON")
             self.pushButton.setText("OFF")
         self.led_state = not self.led_state
+
+    def change_rele_state(self):
+        # Led estaba encendido
+        if self.rele_state:
+            send_command("X")
+            self.label_2.setText("rele: OFF")
+            self.pushButton_2.setText("ON")
+        # Led estaba apagado
+        else:
+            send_command("O")
+            self.label_2.setText("rele: ON")
+            self.pushButton_2.setText("OFF")
+        self.rele_state = not self.rele_state
 
     def slider_moved(self):
         self.lcdNumber.display(self.dial.value())
